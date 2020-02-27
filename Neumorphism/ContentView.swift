3ed2b6db7806cb3b13b0f14fc6e8c57c5fc428bf
Anatolii Zavialov <anatolii.zavialov@daxx-dev.com>
2020-02-27 23:39:19 +0200

    initial commit

---
+++ Neumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by Anatolii Zavialov on 27.02.2020.
+//  Copyright © 2020 Anatolii Zavialov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
